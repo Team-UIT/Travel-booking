463b75a5f84e524f4dbbb34d4839dc160482d3ee
Nhon <nhonpham13@gmail.com>
2021-06-18 00:51:42 +0700

    add model 1

---
+++ backend/model/booking.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+    endAt: {type: Date, required: 'Ending Date is required!'},
+    startAt: {type: Date, required: 'Start Date is required!'},
+    totalPrice: Number,
+    days: Number,
+    guests: Number,
+    createdAt: {type: Date, default: Date.now},
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
+    hotel: {type: Schema.Types.ObjectId, ref: 'Hotel'}
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
